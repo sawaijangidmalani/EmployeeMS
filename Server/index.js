717c9acfd990ea5c35709b9fa21be89707a510cd
Sawai jangid <sawaisingh0706@gmail.com>
2024-08-23 12:48:00 +0530

    commit
    ok

--- Server/index.js
+++ Server/index.js
@@ -6,13 +6,22 @@ import Jwt from "jsonwebtoken";
 import cookieParser from "cookie-parser";
 
 const app = express();
+// app.use(
+//   cors({
+//     origin: ["http://localhost:5173"],
+//     methods: ["GET", "POST", "PUT", "DELETE"],
+//     credentials: true,
+//   })
+// );
+
 app.use(
   cors({
-    origin: ["http://localhost:5173"],
+    origin: ["http://localhost:5173", "https://gtechemployeems.netlify.app"],
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
   })
 );
+
 app.use(express.json());
 app.use(cookieParser());
 app.use("/auth", adminRouter);
