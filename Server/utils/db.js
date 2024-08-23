f10bec9a3a040ae211b708484cb8cb569fc70587
Sawai jangid <sawaisingh0706@gmail.com>
2024-08-23 13:57:37 +0530

    commit

--- Server/utils/db.js
+++ Server/utils/db.js
@@ -17,7 +17,7 @@ const con = mysql.createConnection({
 
 con.connect(function (err) {
   if (err) {
-    console.log("connection error",err);
+    console.log("connection error");
   } else {
     console.log("Connected");
   }
