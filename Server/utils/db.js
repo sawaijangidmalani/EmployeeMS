e9b8b839cd8195e864a0132fc0f50b17dbebc134
Sawai jangid <sawaisingh0706@gmail.com>
2024-08-23 12:45:27 +0530

    commit

--- Server/utils/db.js
+++ Server/utils/db.js
@@ -1,11 +1,19 @@
 import mysql from "mysql2";
 
+// const con = mysql.createConnection({
+//   host: "localhost",
+//   user: "root",
+//   password: "root",
+//   database: "employeems",
+// });
+
 const con = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "root",
-  database: "employeems",
+  host: process.env.DB_HOST || "localhost",
+  user: process.env.DB_USER || "root",
+  password: process.env.DB_PASSWORD || "root",
+  database: process.env.DB_NAME || "employeems",
 });
+
 
 con.connect(function (err) {
   if (err) {
